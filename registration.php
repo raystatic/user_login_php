a18c71c4feabce590835dd012d1c72a886d854e6
raystatic <rahul9650ray@gmail.com>
2019-09-10 12:53:03 +0530

    registration page desingning done!

--- registration.php
+++ registration.php
@@ -2,6 +2,77 @@
 <html>
 <head>
 	<title>User Registration</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+		body{
+			font-family: Arial, Helvetica, sans-serif;
+			background-color: #030303;
+		}
+		* {
+			box-sizing: border-box;
+		}
+		/*Add padding to containers*/
+		.container{
+			padding: 16px;
+			color: white;
+			/*background-color: #4d3434;*/
+		}
+
+		::placeholder{
+			color: #fcfafa;
+		}
+
+		/*Full width input fields*/
+		input[type=text], input[type=password], input[type=email] {
+			width :100%;
+			padding: 15px;
+			margin: 5px 0 22px 0;
+			border-radius: 25px;
+			border: 2px solid white;
+			background-color: #030303;
+			color: white;
+		}
+		
+		input[type=text]:focus, input[type=password]:focus, input[type=email]:focus{
+			background-color: #130e0e;	
+			outline: none;
+		}
+
+		/* overwrite deafult of hr*/
+
+		hr{
+			border: 1px solid #f1f1f1;
+			margin-bottom: 25px;
+		}
+
+		/* set style for submit button*/
+		.registerbutton{
+			background-color: #130e0e;
+			color: white;
+			padding: 16px 20px;
+			border: none;
+			margin-bottom: 20px;
+			cursor: pointer;
+			opacity: 0.9;
+			border-radius: 25px;
+			border: 2px solid white;
+			width: 100%;
+		}
+
+		.registerbutton:hover{
+			opacity: 1
+		}
+
+		.signin{
+			text-align: center;
+		}
+
+		/* Add a blue text color to links */
+		a{
+			color: dodgerblue;
+		}
+
+	</style>
 </head>
 <body>
 
@@ -40,23 +111,31 @@
 	?>
 </div>
 
-<div>
+<div class="container">
 	<form action="registration.php" method="post">
-		<h1>Registration</h1><br>
-		First Name
-		<input type="text" name="firstname" required="true"><br>
-		Last Name
-		<input type="text" name="lastname" required="true"><br>
-		Email
-		<input type="email" name="email" required="true"><br>
-		Phone Number
-		<input type="text" name="phonenumber" required="true"><br>
-		Password
-		<input type="password" name="password" required="true"><br>
+		<h1 align="center">Registration</h1><br>
+		<hr>
+		<label for="firstname"><b>First Name</b></label><br>
+		<input type="text" name="firstname" required="true" placeholder="Enter First Name"><br>
+		<label for="lastname"><b>Last Name</b></label>
+		<input type="text" name="lastname" required="true" placeholder="Enter Last Name"><br>
+		<label for="email"><b>Email</b></label>
+		<input type="email" name="email" required="true" placeholder="Enter Email"><br>
+		<label for="phonenumber"><b>Phone Number</b></label>
+		<input type="text" name="phonenumber" required="true" placeholder="Enter Phone Number"><br>
+		<label for="password"><b>Password</b></label>
+		<input type="password" name="password" required="true" placeholder="Enter Password"><br>
+		<label for="cnfpassword"><b>Confirm Password</b></label>
+		<input type="password" name="cnfpassword" required="true" placeholder="Confirm Password"><br>
 
-		<input type="submit" name="submit" value="Register">
+		<input type="submit" name="submit" value="Register" class="registerbutton">
  		
 	</form>
+
+	<div class="container signin">
+    <p>Already have an account? <a href="login.php">Sign in</a>.</p>
+  </div>
+
 </div>
 
 </body>
